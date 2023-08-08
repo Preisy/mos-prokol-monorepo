8ac1375e548412f2116e4633c74f260f2b65eb43
cbetujihuk <maksmak200305@gmail.com>
2023-08-08 23:47:37 +0300

    + unplugin-vue-router

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -100,7 +100,7 @@ module.exports = configure(function (/* ctx */) {
             imports: [
               // presets
               'vue',
-              'vue-router',
+              VueRouterAutoImports,
               'quasar',
             ],
           },
