d0d32ba6139a5028c3de37b0b84174cee9e6a7b1
cbetujihuk <maksmak200305@gmail.com>
2023-08-09 00:11:47 +0300

    🔧 Unocss fix

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -10,6 +10,7 @@
 
 const { configure } = require('quasar/wrappers');
 const path = require('path');
+const { VueRouterAutoImports } = require('unplugin-vue-router');
 
 module.exports = configure(function (/* ctx */) {
   return {
@@ -105,7 +106,7 @@ module.exports = configure(function (/* ctx */) {
             ],
           },
         ],
-        ['unocss'],
+        ['unocss/vite', {}],
         ['unplugin-vue-macros/vite', {}],
       ],
     },
--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -1,4 +1,4 @@
-import type { Theme } from '@unocss/preset-uno'
+import type { Theme } from '@unocss/preset-uno';
 import {
   defineConfig,
   presetUno,
@@ -6,7 +6,7 @@ import {
   presetIcons,
   transformerVariantGroup,
   transformerDirectives,
-} from 'unocss'
+} from 'unocss';
 
 export default defineConfig<Theme>({
   presets: [
@@ -42,29 +42,5 @@ export default defineConfig<Theme>({
     },
   },
   preflights: [],
-  rules: [
-    ['text-shadowed-white', { 'text-shadow': '2px 4px 10px #FFFFFF' }],
-    [
-      'box-shadow-lg',
-      {
-        'box-shadow':
-          '237px 139px 110px rgba(69, 80, 161, 0.01), 133px 78px 93px rgba(69, 80, 161, 0.05), 59px 35px 69px rgba(69, 80, 161, 0.09), 15px 9px 38px rgba(69, 80, 161, 0.1), 0px 0px 0px rgba(69, 80, 161, 0.1)',
-      },
-    ],
-    [
-      'box-shadow-sm',
-      {
-        'box-shadow':
-          '55px 32px 26px rgba(69, 80, 161, 0.01), 31px 18px 22px rgba(69, 80, 161, 0.05), 14px 8px 16px rgba(69, 80, 161, 0.09), 3px 2px 9px rgba(69, 80, 161, 0.1), 0px 0px 0px rgba(69, 80, 161, 0.1)',
-      },
-    ],
-    [
-      'bg-linear-primary/20',
-      { background: 'linear-gradient(180deg, #B1BBFF -223.61%, rgba(177, 187, 255, 0) 71.88%);' },
-    ],
-    [
-      'bg-linear-primary/50',
-      { background: 'linear-gradient(189.22deg, #9aa7ff 5%, rgba(154, 167, 255, 0) 105.25%), #ffffff;' },
-    ],
-  ],
-})
+  rules: [],
+});
