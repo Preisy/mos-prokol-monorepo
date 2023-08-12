0289657289a849e49240182307404bad693ef855
cbetujihuk <maksmak200305@gmail.com>
2023-08-12 17:42:28 +0300

    💄 unocss colors, app.scss font

--- frontend/src/uno.config.ts
+++ frontend/src/uno.config.ts
@@ -22,16 +22,17 @@ export default defineConfig<Theme>({
   transformers: [transformerVariantGroup(), transformerDirectives()],
   theme: {
     colors: {
-      bg: '#FFF',
-      primary: '#4F62E5',
-      secondary: '#6BDA97',
-      tertiary: '#666666',
+      unset: 'unset',
+      dark: '#1A1A1A',
+      primary: '#FCFCFC',
+      attractive: '#FCE078',
     },
     fontSize: {
-      sm: ['1.25rem', '1.56rem'],
-      base: ['1rem', '1.5rem'],
+      sm: ['0.7rem', 'normal'],
+      base: ['0.9rem', '130%'],
+      md: ['1rem', '140%'],
       lg: ['1.5rem', '1.875rem'],
-      xl: ['3rem', '3.657rem'],
+      xl: ['2.5rem', '140%'],
       '2xl': ['6rem', '7.313rem'],
     },
     breakpoints: {
