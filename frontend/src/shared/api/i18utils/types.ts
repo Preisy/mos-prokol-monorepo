5d826ed4dc9a8acf41f9554e840564111b4d679d
cbetujihuk <maksmak200305@gmail.com>
2023-08-15 17:07:11 +0300

    🌐 add Record<>

--- frontend/src/shared/api/i18utils/types.ts
+++ frontend/src/shared/api/i18utils/types.ts
@@ -2,10 +2,10 @@ export type Locale = 'ru-RU' | 'en-US';
 
 export type I18nMessages = {
   [locale in Locale]: {
-    [key: string]: Messages | string;
+    [key: string]: Messages | string | Record<string, unknown>;
   };
 };
 
 interface Messages {
-  [key: string]: Messages | string;
+  [key: string]: Messages | string | Record<string, unknown>;
 }
