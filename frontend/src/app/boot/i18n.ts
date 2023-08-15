bde882589adcf466e987b79dee0dc1c3d62afa31
cbetujihuk <maksmak200305@gmail.com>
2023-08-15 12:54:49 +0300

    ✨ lodash.merge, mergeI18n

--- frontend/src/app/boot/i18n.ts
+++ frontend/src/app/boot/i18n.ts
@@ -1,32 +1,12 @@
 import { boot } from 'quasar/wrappers';
 import { createI18n } from 'vue-i18n';
-
-// eslint-disable-next-line boundaries/element-types
-import messages from 'app/i18n';
-
-export type MessageLanguages = keyof typeof messages;
-// Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = (typeof messages)['en-US'];
-
-// See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
-/* eslint-disable @typescript-eslint/no-empty-interface */
-declare module 'vue-i18n' {
-  // define the locale messages schema
-  export interface DefineLocaleMessage extends MessageSchema {}
-
-  // define the datetime format schema
-  export interface DefineDateTimeFormat {}
-
-  // define the number format schema
-  export interface DefineNumberFormat {}
-}
-/* eslint-enable @typescript-eslint/no-empty-interface */
+import { mergeI18n } from 'shared/api/i18utils';
 
 export default boot(({ app }) => {
   const i18n = createI18n({
     locale: 'en-US',
     legacy: false,
-    messages,
+    messages: mergeI18n(),
   });
 
   // Set i18n instance on app
