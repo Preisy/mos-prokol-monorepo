3ceb07a6f2fde596796f06dcc58de1bad2ac84d2
CBETuJIHuK <maksmak200305@gmail.com>
2023-08-26 19:39:40 +0300

    💄 Better about us

--- frontend/src/shared/ui/SPrettyHeader/ui/SPrettyHeader.vue
+++ frontend/src/shared/ui/SPrettyHeader/ui/SPrettyHeader.vue
@@ -2,15 +2,15 @@
 
 <template>
   <div inline-block uppercase relative class="s-pretty-header">
-    <h1 text-xl font-700 text-white relative z-1><slot></slot></h1>
+    <h1 text-12 font-700 text-white relative z-1><slot></slot></h1>
     <h1
       font-700
       blur-1
-      text-14
+      text-12
       text-black
       absolute
-      left-8
-      top--14
+      left-4
+      top--4
       whitespace-nowrap
       text-outline-attractive-2
     >
--- frontend/src/widgets/WAboutUs/ui/WAboutUs.vue
+++ frontend/src/widgets/WAboutUs/ui/WAboutUs.vue
@@ -1,29 +1,40 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import { SPrettyHeader } from 'shared/ui/SPrettyHeader';
 import { SStructure } from 'shared/ui/SStructure';
+
+const { tm } = useI18n();
 </script>
 <template>
   <div class="w-about-us" bg-black overflow-hidden relative mt--16>
     <SStructure py-40 relative z-1>
       <SPrettyHeader>{{ $t('about.header') }}</SPrettyHeader>
-      <div w="6/10" text-white>
-        <div mb-6>
-          {{ $t('about.desc.0') }}
-        </div>
-        <div>
-          {{ $t('about.desc.1') }}
+      <div w="6/10" text-white font-600>
+        <div v-for="(line, lineIndex) of tm('about.desc')" :key="lineIndex">
+          <span v-html="line"></span>
         </div>
       </div>
     </SStructure>
     <img
       src="/widgets/WAboutUs/background.png"
       absolute
-      w-280
       z-0
-      brightness-50
-      right--80
+      brightness-70
       top--8
       rounded="5/10"
+      h="11/10"
+      w-auto
+      right-0
+      translate-x="4/10"
     />
   </div>
 </template>
+
+<style scoped lang="scss">
+.w-about-us:deep(strong) {
+  --uno: text-attractive-2;
+}
+.w-about-us:deep(li::marker) {
+  --uno: text-attractive-2;
+}
+</style>
