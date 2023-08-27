e50866f5d4c089a8310eebb12ce25d7e64fa91bd
CBETuJIHuK <maksmak200305@gmail.com>
2023-08-27 13:33:33 +0300

    🎨 WStages better img height

--- frontend/src/widgets/WStages/ui/StageDisplay.vue
+++ frontend/src/widgets/WStages/ui/StageDisplay.vue
@@ -9,7 +9,7 @@ const imgUrl = computed(() => `/stages/${props.id}.png`);
 
 <template>
   <div w-full h-full overflow-hidden rounded-tr-2.4 rounded-br-2.4 relative>
-    <q-img fit="cover" :src="imgUrl" />
+    <img :src="imgUrl" w-full h-full />
     <DisplayCard absolute bottom-8 left-auto right-auto mx-4>
       <slot></slot>
     </DisplayCard>
--- frontend/src/widgets/WStages/ui/StagesPresentation.vue
+++ frontend/src/widgets/WStages/ui/StagesPresentation.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { StageI18n } from './i18n';
+import { StageI18n } from '../i18n';
 import StageCard from './StageCard.vue';
 import StageDisplay from './StageDisplay.vue';
 
@@ -9,11 +9,19 @@ const { tm } = useI18n();
 
 const currentActive = ref(1);
 const stagesTextes = tm('stages.data') as Array<StageI18n>;
+const cardsRef = ref<HTMLElement>();
+const displayRef = ref<HTMLElement>();
+
+onMounted(() => {
+  if (!cardsRef.value || !displayRef.value) return;
+  const rect = cardsRef.value.getBoundingClientRect();
+  displayRef.value.style.height = `${rect?.height}px`;
+});
 </script>
 
 <template>
   <div grid grid-cols-2 w-fit mx-auto justify-center>
-    <div class="cards">
+    <div ref="cardsRef" class="cards" justify-self-end>
       <StageCard
         v-for="(stage, index) in stagesTextes"
         :key="index"
@@ -25,7 +33,7 @@ const stagesTextes = tm('stages.data') as Array<StageI18n>;
         {{ stage.cardText }}
       </StageCard>
     </div>
-    <div class="display" w-full>
+    <div ref="displayRef" class="display" w-full>
       <StageDisplay :id="currentActive">
         {{ stagesTextes[currentActive].displayText }}
       </StageDisplay>
