aee89ecfc3529bbb78ef21f7ead316658b7b204e
CBETuJIHuK <maksmak200305@gmail.com>
2023-08-30 17:57:01 +0300

    💫 WStages animation

--- frontend/src/widgets/WStages/ui/StageCard.vue
+++ frontend/src/widgets/WStages/ui/StageCard.vue
@@ -25,20 +25,32 @@ const textId =
     flex
     flex-row
     w-27rem
+    cursor-pointer
+    border-solid
+    border-0
     class="card"
     :class="{
       'rounded-tl-0.6rem': first,
       'rounded-bl-0.6rem': last,
-      'w-28rem ml--1rem rounded-l-0.6rem border-attractive2 border-l-6 border-0 border-solid [&>.index]:text-attractive2 [&>.desc]:text-black':
+      'w-28rem ml--1rem rounded-l-0.6rem border-attractive2 border-l-6 [&>.index]:text-attractive2 [&>.desc]:text-black':
         id === currentIndex,
     }"
     @click="$emit('update:currentIndex', id)"
   >
-    <div class="index" text-xl line-height-none font-700 m-r-8.8>
+    <div class="index" text-xl line-height-none font-700 m-r-8.8 transition-300>
       {{ textId }}
     </div>
-    <div class="desc" text-md uppercase font-600>
+    <div class="desc" text-md uppercase font-600 transition-300>
       <slot></slot>
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.card {
+  will-change: border, border-radius, margin, width;
+  transition-delay: 0ms;
+  transition-duration: 300ms;
+  transition-property: border, border-radius, margin, width;
+}
+</style>
--- frontend/src/widgets/WStages/ui/StageDisplay.vue
+++ frontend/src/widgets/WStages/ui/StageDisplay.vue
@@ -4,12 +4,12 @@ import DisplayCard from './DisplayCard.vue';
 const props = defineProps<{
   id: number;
 }>();
-const imgUrl = computed(() => `widgets/WStages/${props.id}.png`);
+const imgUrl = computed(() => `/widgets/WStages/${props.id}.png`);
 </script>
 
 <template>
   <div w-full h-full overflow-hidden rounded-tr-2.4 rounded-br-2.4 relative>
-    <img :src="imgUrl" w-full h-full />
+    <q-img :src="imgUrl" w-full h-full />
     <DisplayCard absolute bottom-8 left-auto right-auto mx-4>
       <slot></slot>
     </DisplayCard>
